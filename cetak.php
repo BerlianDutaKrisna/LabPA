4e8264afa6a8ae5fafc58318a65d744d8b17ecdf
BerlianDutaKrisna <berliandutakrisna@gmail.com>
2024-07-20 12:27:30 +0700

    form cetak

--- cetak.php
+++ cetak.php
@@ -3,11 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Table A4 Size</title>
+    <title>Table F4 Size</title>
     <style>
         @page {
-            size: A4;
-            margin: 20mm;
+            size: 215mm 330mm; /* F4 size */
         }
 
         body {
@@ -19,129 +18,106 @@
         table {
             width: 100%;
             border-collapse: collapse;
-            margin-bottom: 20px;
-            table-layout: fixed;
-            height: calc(100vh - 40mm);
         }
-
-        th, td {
+         td {
             border: 1px solid #000;
-            
             text-align: left;
-            height: 50px;
+            padding: 4px;
         }
-
-        thead th {
-            background-color: #f2f2f2;
+        .judul{
+            font-weight: bold;
         }
-
-        tfoot td {
-            border: 1px solid #000;
+        .text {
+            padding: 150px;
         }
-
-        .no-border {
-            border: 1px solid #000;
-        }
-
-        /* Ensuring the table fills the full height of the page */
-        tbody, tfoot {
-            display: table-row-group;
-        }
-
-        tbody tr, tfoot tr {
-            height: auto;
-        }
-
-        tbody tr {
-            height: calc((100vh - 40mm) / 10); /* Assuming roughly 10 rows */
+        .gambar {
+            padding: 50px;            
         }
     </style>
 </head>
 <body>
     <table>
-        <thead>
-            <tr>
-                <th colspan="2">Kode HPA :</th>
-                <th colspan="2">Tanggal Mengerjakan :</th>
-                <th colspan="2">APD :</th>
-                <th colspan="2">Analis Mengerjakan :</th>
-            </tr>
-        </thead>
         <tbody>
             <tr>
-                <td colspan="8">Makroskopis</td>
+                <td colspan="1">Kode HPA :</td>
+                <td colspan="1">Tanggal Mengerjakan :</td>
+                <td colspan="1">Tanggal Janji Hasil :</td>
+                <td colspan="1">APD :</td>
+            </tr>
+            <tr class="judul">
+                <td colspan="2" class="judul" >Makroskopis</td>
+                <td colspan="2" >Analis PA :</td>
             </tr>
             <tr>
-                <td colspan="8"></td>
+                <td colspan="4" class="text"></td>
+            </tr>
+            <tr class="judul">
+                <td colspan="2">Mikroskopis</td>
+                <td colspan="2">Dokter PA :</td>
             </tr>
             <tr>
-                <td colspan="8">Mikroskopis</td>
+                <td colspan="4" class="text"></td>
+            </tr>
+            <tr class="judul">
+                <td colspan="2">Traking Spesimen</td>
+                <td colspan="2">Kualitas Sediaan</td>
             </tr>
             <tr>
-                <td colspan="8"></td>
+                <td colspan="2">1. Bahan Diterima tgl...</td>
+                <td colspan="2">1. Volume cairan fiksasi sesuai...</td>
             </tr>
             <tr>
-                <td colspan="4">Traking Spesimen</td>
-                <td colspan="4">kualitas_sediaan</td>
+                <td colspan="2">2. Makroskopis tgl...</td>
+                <td colspan="2">2. Jaringan terfiksasi merata...</td>
             </tr>
             <tr>
-                <td colspan="4" >1. Bahan Diterima tgl...</td>
-                <td colspan="4">1. Volume cairan fiksasi sesuai...</td>
+                <td colspan="2">3. Prosessong tgl...</td>
+                <td colspan="2">3. Blok parafin tidak ada fragmentasi...</td>
             </tr>
             <tr>
-                <td colspan="4">2. Makroskopis tgl...</td>
-                <td colspan="4">2. Jaringan terfiksasi merata...</td>
+                <td colspan="2">4. Embeding (HPA) tgl...</td>
+                <td colspan="2">4. Sediaan tanpa lipatan...</td>
             </tr>
             <tr>
-                <td colspan="4">3. Prosessong tgl...</td>
-                <td colspan="4">3. Blok parafin tidak ada fragmentasi...</td>
+                <td colspan="2">5. Mikrotomi (HPA) tgl...</td>
+                <td colspan="2">5. sediaan tanpa goresan mata pisau...</td>
             </tr>
             <tr>
-                <td colspan="4">4. Embeding (HPA) tgl...</td>
-                <td colspan="4">4. Sediaan tanpa lipatan...</td>
+                <td colspan="2">6. Pewarnaan tgl...</td>
+                <td colspan="2">6. Kontras warna sediaan cukup jelas...</td>
             </tr>
             <tr>
-                <td colspan="4">5. Mikrotomi (HPA) tgl...</td>
-                <td colspan="4">5. sediaan tanpa goresan mata pisau...</td>
+                <td colspan="2">7. Entelan tgl...</td>
+                <td colspan="2">7. Sediaan tanpa gelembung...</td>
             </tr>
             <tr>
-                <td colspan="4">6. Pewarnaan tgl...</td>
-                <td colspan="4">6. Kontras warna sediaan cukup jelas...</td>
-            </tr>
-            <tr>
-                <td colspan="4">7. Entelan tgl...</td>
-                <td colspan="4">7. Sediaan tanpa gelembung...</td>
-            </tr>
-            <tr>
-                <td colspan="4"></td>
-                <td colspan="4">8. Sediaan tanpa bercak/sidik jari...</td>
+                <td colspan="2">8. Selesai tgl...</td>
+                <td colspan="2">8. Sediaan tanpa bercak/sidik jari...</td>
             </tr>
         </tbody>
-        <tfoot>
-            <tr>
-                <td colspan="8">Gambar</td>
+    </table>
+    <table>
+        <tbody>
+            <tr class="judul">
+                <td colspan="6">Gambar</td>
             </tr>
             <tr>
-                <td colspan="1"></td>
-                <td colspan="1"></td>
-                <td colspan="1"></td>
-                <td colspan="1"></td>
-                <td colspan="1"></td>
-                <td colspan="1"></td>
-                <td colspan="1"></td>
-                <td colspan="1"></td>
+                <td colspan="1" class="gambar"></td>
+                <td colspan="1" class="gambar"></td>
+                <td colspan="1" class="gambar"></td>
+                <td colspan="1" class="gambar"></td>
+                <td colspan="1" class="gambar"></td>
+                <td colspan="1" class="gambar"></td>
             </tr>
             <tr>
-                <td colspan="1"></td>
-                <td colspan="1"></td>
-                <td colspan="1"></td>
-                <td colspan="1"></td>
-                <td colspan="1"></td>
-                <td colspan="1"></td>
-                <td colspan="1"></td>
-                <td colspan="1"></td>
+                <td colspan="1" class="gambar"></td>
+                <td colspan="1" class="gambar"></td>
+                <td colspan="1" class="gambar"></td>
+                <td colspan="1" class="gambar"></td>
+                <td colspan="1" class="gambar"></td>
+                <td colspan="1" class="gambar"></td>
             </tr>
-        </tfoot>
+        </tbody>
     </table>
 </body>
 </html>
